7078c735ffbd463ed5f221c295bcc205cc97d5b3
liamshatzel <liam.shatzel@gmail.com>
2023-12-05 21:51:01 -0800

    p2

--- day6/day6.cpp
+++ day6/day6.cpp
@@ -45,62 +45,37 @@ int main() {
     while(getline(times, cur_time, ' ')){
         if(cur_time != "" && cur_time != "\n" && isdigit(cur_time[0])){
             new_cur_time += cur_time;
-            cur_times.push_back(stoi(cur_time));
+            cur_times.push_back(stoll(cur_time));
         }
 
     }
     while(getline(dists, cur_dist, ' ')){
         if(cur_dist != "" && cur_dist != "\n" && isdigit(cur_dist[0])){
-            cur_dists.push_back(stoi(cur_dist));
+            cur_dists.push_back(stoll(cur_dist));
             new_cur_dist += cur_dist;
         }
     }
-
-    cout << new_cur_time << endl;
-    cout << new_cur_dist << endl;
+    unsigned long long new_tot_time = stoll(new_cur_time);
+    unsigned long long new_tot_dist = stoll(new_cur_dist);
 
     vector<int> tot_wins; 
-    for(int i = 0; i < cur_times.size(); i++){
-        int tot_time = cur_times[i];
+        int tot_time = new_tot_time;
         int win_ways = 0;
-        //cout << tot_time << endl;
-        for(int j = 0; j < tot_time; j++){
-            int does_win = (tot_time - j) * j;
-            if(does_win > cur_dists[i]){
+
+        for(unsigned long long j = 0; j < tot_time; j++){
+            unsigned long long does_win = (tot_time - j) * j;
+            if(does_win > new_tot_dist){
                 win_ways++;
             }
         }
         tot_wins.push_back(win_ways);
-    }
     
     int total_score = 1;
     for(int win : tot_wins){
-        //cout << win << endl;
         if(win != 0)
             total_score *= win;
     }
 
     cout << total_score << endl;
 
-    
-
-
-
-    //while(getline(cin, line)){
-    //    stringstream ss(line);
-    //    for()
-    //    getline(ss, time, ':');
-    //    getline(ss, distance, ':');
-    //    stringstream number_line(game_num);
-
-    //    int j = 0;
-
-    //    //get game line and number
-    //    while(getline(number_line, number, ' ')){
-    //        if(j > 0){
-    //            game_num = number;
-    //        }
-    //        j++;
-    //    }
-    //}
 }
